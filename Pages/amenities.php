e12850f86c44aec5e263cacc6adc52b097bb60a2
Bartolome, Louise Anne S. <louisebartolome.basc@gmail.com>
2025-04-02 14:26:30 +0800

    git commit

---
+++ Pages/amenities.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mamyr - Amenities</title>
+    <link rel="stylesheet" href="../assets/css/amenities.css">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+            <button class=" navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse " id="navbarNav">
+                <img src="../assets/images/MamyrLogo.png" alt="" class="logoNav">
+
+                <ul class="navbar-nav ms-auto me-10">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="amenities.php">AMENITIES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BLOG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">RATES</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BE OUR PARTNER</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BOOK NOW</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="amenities">
+
+        <h1 class="title">Our Amenities</h1>
+
+        <div class="embed-responsive embed-responsive-16by9">
+            <video id="mamyrVideo" autoplay muted loop controls class="embed-responsive-item">
+                <source src="../assets/videos/mamyrVideo1.mp4" type="video/mp4">
+            </video>
+
+        </div>
+
+        <div class="pool">
+            <div class="poolTitleContainer">
+                <hr class="poolLine">
+                <h4 class="poolTitle">Swimming Pools</h4>
+                <p class="poolDescription">We offer three spacious pools designed for relaxation and fun. Whether you’re
+                    looking to take a
+                    refreshing dip or lounge by the water, each pool provides a perfect setting to unwind and enjoy your
+                    stay. Dive in and make the most of your resort experience!</p>
+            </div>
+
+            <div class="poolPics">
+                <img src="../assets/images/amenities/poolPics/poolPic1.png" alt="Pool Picture 1" class="pic1">
+                <img src="../assets/images/amenities/pavilionPics/pav1.jpg" alt="Pool Picture 2" class="pic2">
+                <img src="../assets/images/amenities/poolPics/poolPic3.jpg" alt="Pool Picture 3" class="pic3">
+                <img src="../assets/images/amenities/poolPics/poolPic4.jpg" alt="Pool Picture 4" class="pic4">
+                <img src="../assets/images/amenities/poolPics/poolPic5.jpg" alt="Pool Picture 5" class="pic5">
+            </div>
+        </div>
+
+        <div class="pool">
+            <div class="poolTitleContainer">
+                <hr class="poolLine">
+                <h4 class="poolTitle">Cottages</h4>
+                <p class="poolDescription">Our cozy cottages offer a relaxing retreat with spacious porches, secure
+                    surroundings, and a refreshing ambiance. Enjoy a perfect blend of nature and modern facilities
+                    designed for your comfort.</p>
+            </div>
+
+            <div class="poolPics">
+                <img src="../assets/images/amenities/cottagePics/cottage1.jpg" alt="Cottage Picture 1" class="pic1">
+                <img src="../assets/images/amenities/cottagePics/cottage2.jpg" alt="Cottage Picture 2" class="pic2">
+            </div>
+        </div>
+
+        <div class="pool">
+            <div class="poolTitleContainer">
+                <hr class="poolLine">
+                <h4 class="poolTitle">Pavilion Hall</h4>
+                <p class="poolDescription">Our Pavilion Hall offers the perfect space for events, gatherings, and
+                    special occasions. With its spacious, elegant design and breathtaking views, it’s ideal for
+                    everything from weddings to corporate events. Let us help you create unforgettable memories in a
+                    setting of pure sophistication.</p>
+            </div>
+
+            <div class="poolPics">
+                <img src="../assets/images/amenities/pavilionPics/pav1.jpg" alt="Pavilion Picture 1" class="pic1">
+                <img src="../assets/images/amenities/pavilionPics/pav2.jpg" alt="Pavilion Picture 2" class="pic2">
+                <img src="../assets/images/amenities/pavilionPics/pav3.jpg" alt="Pavilion Picture 3" class="pic3">
+                <img src="../assets/images/amenities/pavilionPics/pav4.jpg" alt="Pavilion Picture 4" class="pic4">
+                <img src="../assets/images/amenities/pavilionPics/pav5.jpg" alt="Pavilion Picture 5" class="pic5">
+
+            </div>
+        </div>
+
+        <div class="pool">
+            <div class="poolTitleContainer">
+                <hr class="poolLine">
+                <h4 class="poolTitle">Mamyr Hotel</h4>
+                <p class="poolDescription">We offer 12 thoughtfully designed hotel rooms, each providing a peaceful and
+                    comfortable retreat. Perfect for guests looking for a relaxing space to unwind after a day of
+                    exploration, our rooms offer all the essentials for a restful stay with a touch of convenience.</p>
+            </div>
+
+            <div class="poolPics">
+                <img src="../assets/images/amenities/hotelPics/hotel1.jpg" alt="Hotel Picture 1" class="pic1">
+                <img src="../assets/images/amenities/hotelPics/hotel2.jpg" alt="Hotel Picture 2" class="pic1">
+                <img src="../assets/images/amenities/hotelPics/hotel3.jpg" alt="Hotel Picture 3" class="pic3">
+                <img src="../assets/images/amenities/hotelPics/hotel4.jpg" alt="Hotel Picture 4" class="pic4">
+
+
+
+
+            </div>
+        </div>
+        <div class="pool">
+            <div class="poolTitleContainer">
+                <hr class="poolLine">
+                <h4 class="poolTitle">Parking Space</h4>
+                <p class="poolDescription">We provide ample parking spaces to ensure a hassle-free stay. Whether you’re
+                    arriving by car or with a group, our secure parking area is conveniently located, giving you peace
+                    of mind throughout your visit.</p>
+            </div>
+
+            <div class="poolPics">
+                <img src="../assets/images/amenities/parkingPics/parking1.jpg" alt="Parking Picture 1" class="pic1">
+                <img src="../assets/images/amenities/parkingPics/parking2.jpg" alt="Parking Picture 2" class="pic2">
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <footer class="py-1 my-2">
+        <div class=" pb-1 mb-1 d-flex align-items-center justify-content-start">
+
+            <img src="../assets/images/MamyrLogo.png" alt="Mamyr Resort and Events Place" class="logo">
+
+            <h3 class="mb-0">MAMYR RESORT AND EVENTS PLACE</h3>
+        </div>
+
+        <div class="info">
+            <div class="reservation">
+                <h4 class="reservationTitle">Reservation</h4>
+                <h4 class="numberFooter">(0998) 962 4697 </h4>
+                <h4 class="emailAddressTextFooter">mamyr@gmail.com</h4>
+            </div>
+            <div class="locationFooter">
+                <h4 class="locationTitle">Location</h4>
+                <h4 class="addressTextFooter">Sitio Colonia, Gabihan, San Ildefonso, Bulacan</h4>
+
+            </div>
+        </div>
+        <hr class="footerLine">
+        <div class="socialIcons">
+            <a href="https://www.facebook.com/p/Mamyr-Resort-Restaurant-Events-Place-100083298304476/"><i
+                    class='bx bxl-facebook-circle'></i></a>
+            <a href="https://workspace.google.com/intl/en-US/gmail/"><i class='bx bxl-gmail'></i></a>
+            <a href="tel:+09989624697">
+                <i class='bx bxs-phone'></i>
+            </a>
+
+        </div>
+
+    </footer>
+
+
+    <script>
+    var video = document.getElementById("myVideo");
+
+    video.onplay = function() {
+        video.muted = false;
+    };
+    </script>
+
+</body>
+
+</html>
