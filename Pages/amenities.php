6d8a3035cb262fbf234d6178c12e7e535fbb070b
Bartolome, Louise Anne S. <louisebartolome.basc@gmail.com>
2025-05-06 19:27:30 +0800

    FENavandotherchanges

--- Pages/amenities.php
+++ Pages/amenities.php
@@ -59,7 +59,7 @@ require '../Config/dbcon.php';
                     <a class="nav-link" href="register.php">BOOK NOW</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="Pages/register.php">Sign Up</a>
+                    <a class="nav-link" href="register.php">Sign Up</a>
                 </li>
             </ul>
         </div>
