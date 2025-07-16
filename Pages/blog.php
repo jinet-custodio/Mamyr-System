e86d6b6e533094390a00fad39459619ae481748a
jinet-custodio <jeanettemaecustodio.basc@gmail.com>
2025-07-17 00:01:44 +0800

    added view post modal, edited landing page style

--- Pages/blog.php
+++ Pages/blog.php
@@ -10,19 +10,19 @@ require '../Config/dbcon.php'
     <title>Mamyr - Blog</title>
     <link rel="icon" type="image/x-icon" href="../assets/Images/Icon/favicon.png ">
     <link rel="stylesheet" href="../Assets/CSS/blog.css">
-    <link rel="stylesheet" href="../Assets/CSS/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="../Assets/CSS/bootstrap.min.css"> -->
+    <!-- link for online bootstrap CDN  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- icon libraries from font-awesome and box icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
 </head>
 
 
 <body>
-    <nav class="navbar navbar-expand-lg fixed-top" id="navbar-half2">
+    <nav class="navbar navbar-expand-lg fixed-top" id="navbar-half2" style="background-color: white;">
         <button class=" navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -100,8 +100,6 @@ require '../Config/dbcon.php'
             }
         }
 
-
-
         uasort($blogPosts, function ($a, $b) {
             $dateA = isset($a['EventDate']) ? $a['EventDate'] : '0000-00-00';
             $dateB = isset($b['EventDate']) ? $b['EventDate'] : '0000-00-00';
@@ -122,7 +120,6 @@ require '../Config/dbcon.php'
             <div class="title">
                 <h5>Recent blog posts</h5>
             </div>
-
             <div class="posts">
                 <!-- Featured Post -->
                 <?php if (!empty($firstPost)): ?>
@@ -133,13 +130,13 @@ require '../Config/dbcon.php'
 
                             if ($featuredContentID && isset($imagesByContentID[$featuredContentID])) {
                                 $imgData = $imagesByContentID[$featuredContentID][0]['imageData'];
-                                $altText = $imagesByContentID[$featuredContentID][0]['altText'] ?? 'Blog image';
+                                $featuredAlt = $imagesByContentID[$featuredContentID][0]['altText'] ?? 'Blog image';
                                 $finfo = finfo_open();
                                 $mimeType = finfo_buffer($finfo, $imgData, FILEINFO_MIME_TYPE);
                                 finfo_close($finfo);
 
-                                $base64Image = base64_encode($imgData);
-                                echo "<img src='data:$mimeType;base64,$base64Image' alt='" . htmlspecialchars($altText) . "' />";
+                                $featuredImage = base64_encode($imgData);
+                                echo "<img src='data:$mimeType;base64,$featuredImage' alt='" . htmlspecialchars($featuredAlt) . "' />";
                             } else {
                                 echo "<img src='../Assets/Images/no-picture.jpg' alt='Default blog image'>";
                             }
@@ -163,6 +160,10 @@ require '../Config/dbcon.php'
                                 <div class="blogDescription">
                                     <?= htmlspecialchars($firstPost['Content'] ?? '') ?>
                                 </div>
+                                <button class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#modalFeatured">
+                                    Read More
+                                </button>
+
                             </div>
                         </div>
                     </div>
@@ -211,10 +212,117 @@ require '../Config/dbcon.php'
                                 <div class="othersDescription">
                                     <?= htmlspecialchars($post['Content'] ?? '') ?>
                                 </div>
+                                <button class="btn btn-primary mt-3" style="display:flex;align-self:flex-end;text-align:center" data-bs-toggle="modal" data-bs-target="#modal<?= htmlspecialchars($postID) ?>">
+                                    Read More
+                                </button>
                             </div>
                         </div>
                     <?php } ?>
                 </div>
+
+                <!-- Modal for featured post -->
+                <div class="modal fade" id="modalFeatured" tabindex="-1" aria-labelledby="modalFeaturedLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-scrollable">
+                        <div class="modal-content">
+
+                            <div class="modal-header py-4">
+                                <h5 class="modal-title" id="modalFeaturedLabel">
+                                    <?= htmlspecialchars($firstPost['EventHeader'] ?? 'Blog Post') ?>
+                                </h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+
+                            <div class="modal-body">
+
+                                <?php
+
+                                $featuredContentID = $firstPost['contentID'] ?? null;
+                                $featuredImage = '../../Assets/Images/no-picture.jpg'; // default fallback
+                                $featuredAlt = 'Blog image';
+
+                                if ($featuredContentID && isset($imagesByContentID[$featuredContentID][0])) {
+                                    $imgData = $imagesByContentID[$featuredContentID][0]['imageData'];
+                                    $featuredAlt = $imagesByContentID[$featuredContentID][0]['altText'] ?? 'Blog image';
+                                    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                                    $mimeType = finfo_buffer($finfo, $imgData);
+                                    finfo_close($finfo);
+                                    $featuredImage = 'data:' . $mimeType . ';base64,' . base64_encode($imgData);
+                                }
+                                ?>
+
+                                <img src="<?= htmlspecialchars($featuredImage) ?>" alt="<?= htmlspecialchars($featuredAlt) ?>" class="img-fluid mb-3" />
+                                <?php if (isset($firstPost['EventType'], $firstPost['EventDate'])): ?>
+                                    <p class="text-muted">
+                                        <?= htmlspecialchars($firstPost['EventType']) ?> • <?= htmlspecialchars(date("j F Y", strtotime($firstPost['EventDate']))) ?>
+                                    </p>
+
+                                <?php endif; ?>
+                                <div class="blog-full-content">
+                                    <?= nl2br(htmlspecialchars($firstPost['Content'] ?? '')) ?>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal for other posts -->
+                <?php foreach ($blogPosts as $postID => $post): ?>
+                    <?= "<!-- DEBUG: modal ID = modal$postID -->"; ?>
+                    <?php
+                    $contentID = $post['contentID'] ?? null;
+                    $image = null;
+                    $alt = 'Blog image';
+
+                    if ($contentID && isset($imagesByContentID[$contentID][0])) {
+                        $imgData = $imagesByContentID[$contentID][0]['imageData'];
+                        $alt = $imagesByContentID[$contentID][0]['altText'] ?? 'Blog image';
+                        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+                        $mimeType = finfo_buffer($finfo, $imgData);
+                        finfo_close($finfo);
+                        $base64Image = 'data:' . $mimeType . ';base64,' . base64_encode($imgData);
+                    } else {
+                        $base64Image = '../../Assets/Images/no-picture.jpg'; // fallback
+                    }
+                    ?>
+
+                    <div class="modal fade" id="modal<?= htmlspecialchars($postID) ?>" tabindex="-1" aria-labelledby="modalLabel<?= htmlspecialchars($postID) ?>" aria-hidden="true">
+                        <div class="modal-dialog modal-lg modal-dialog-scrollable">
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalLabel<?= htmlspecialchars($postID) ?>">
+                                        <?= htmlspecialchars($post['EventHeader'] ?? 'Blog Post') ?>
+                                    </h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+
+                                <div class="modal-body">
+                                    <!-- Blog Image -->
+                                    <img src="<?= htmlspecialchars($base64Image) ?>" alt="<?= htmlspecialchars($alt) ?>" class="img-fluid mb-3" />
+                                    <!-- Event Info -->
+                                    <?php if (isset($post['EventType'], $post['EventDate'])): ?>
+                                        <p class="text-muted">
+                                            <?= htmlspecialchars($post['EventType']) ?> • <?= htmlspecialchars(date("j F Y", strtotime($post['EventDate']))) ?>
+                                        </p>
+                                    <?php endif; ?>
+                                    <!-- Full Content -->
+                                    <div class="blog-full-content">
+                                        <?= nl2br(htmlspecialchars($post['Content'] ?? '')) ?>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+
             </div>
         </div>
     </main>
