433150b02fa3fed4b0234b6597cfe74544bc75ff
Bartolome, Louise Anne S. <louisebartolome.basc@gmail.com>
2025-08-30 18:02:35 +0800

    beOurPartnerNew

---
+++ Pages/beOurPartnerNew.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mamyr Resort And Events Place - Be Our Partner</title>
+    <link rel="icon" type="image/x-icon" href="../Assets/Images/Icon/favicon.png ">
+    <link rel="stylesheet" href="../Assets/CSS/beOurPartnerNew.css">
+    <!-- Link to Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
+</head>
+
+<body>
+    <!-- <?php if ($editMode): ?>
+    <button id="saveChangesBtn" class="btn btn-success">Save Changes</button>
+    <?php endif; ?>
+    <?php if (!$editMode): ?> -->
+    <nav class="navbar navbar-expand-lg fixed-top" id="navbar">
+        <button class=" navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <img src="../Assets/Images/MamyrLogo.png" alt="Mamyr Resort Logo" class="logoNav">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto me-10" id="toggledNav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php"> Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link  dropdown-toggle " href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        AMENITIES
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item active" href="amenities.php">RESORT AMENITIES</a></li>
+                        <li><a class="dropdown-item" href="ratesAndHotelRooms.php">RATES AND HOTEL ROOMS</a></li>
+                        <li><a class="dropdown-item" href="events.php">EVENTS</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/Pages/blog.php">BLOG</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="/Pages/busPartnerRegister.php" id="bopNav">BE OUR PARTNER</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./about.php">ABOUT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">BOOK NOW</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php">Sign Up</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- <?php endif; ?> -->
+
+    <section class="topSec">
+        <div class="topLeft">
+            <h6 class="topText">Become Mamyr's Business Partner</h6>
+
+            <h2 class="headerText">Unlock New Opportunities with Mamyr Resort & Events Place as Your Business Partner.
+            </h2>
+            <h5 class="subtext">Let's work together. Collaborate with us to grow your business and to better serve
+                mutual customers.</h5>
+
+            <a href="busPartnerRegister.php" class="btn btn-primary" id="applyasPartner">Apply as Partner</a>
+        </div>
+
+        <div class="topRight">
+            <img src="../Assets/Images/beOurPartnerPhotos/bpIcon.png" alt="BP Icon" class="bpIcon">
+        </div>
+    </section>
+
+    <section class="middleSec">
+
+        <div class="partnershipContainer" id="partnershipContainer">
+            <div class="partnershipTitleContainer">
+                <h3 class="partnershipTitle">Partner Services</h3>
+
+                <p class="partnershipDescription indent">Mamyr Resort and Events Place is open to collaborating with
+                    trusted services and businesses, offering opportunities for partnership across various
+                    event-related services that contribute to creating memorable and seamless celebrations.</p>
+            </div>
+
+
+            <div class="partnerIconContainer">
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/photog.png" alt="Photography Icon"
+                        class="partnerIcon">
+                    <h4 class="partnerTitle">Photography/Videography</h4>
+                </div>
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/sound.png" alt="Sound/Light Icon" class="partnerIcon">
+                    <h4 class="partnerTitle">Sound and Lighting</h4>
+                </div>
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/host.png" alt="Host Icon" class="partnerIcon">
+                    <h4 class="partnerTitle">Event Hosting</h4>
+                </div>
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/photoBooth.png" alt="Photo Booth Icon"
+                        class="partnerIcon">
+                    <h4 class="partnerTitle">Photo Booth</h4>
+                </div>
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/perf.png" alt="Performer Icon" class="partnerIcon">
+                    <h4 class="partnerTitle">Performer</h4>
+                </div>
+
+                <div class="partnerServiceContainer">
+                    <img src="../Assets/Images/beOurPartnerPhotos/foodCart.png" alt="Food Cart Icon"
+                        class="partnerIcon">
+                    <h4 class="partnerTitle">Food Cart</h4>
+                </div>
+            </div>
+    </section>
+
+    <section class="bottomSec">
+        <div class="partnershipTitleContainer">
+            <h3 class="partnershipTitle">Our Featured Partners </h3>
+
+            <p class="partnershipDescription indent">Mamyr Resort and Events Place is proud to collaborate with a select
+                group of trusted businesses and services that help us create unforgettable and seamless events. These
+                valued partners
+                provide a range of services that contribute to making every celebration special.</p>
+        </div>
+
+
+        <div class="BPContainer">
+
+            <div class="card bp-card" id="bp1">
+                <img src="../Assets/Images/no-picture.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Singko Marias</h5>
+                    <h6 class="card-subtitle">Photography</h6>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, ea aliquid.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card bp-card" id="bp1">
+                <img src="../Assets/Images/no-picture.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Singko Marias</h5>
+                    <h6 class="card-subtitle">Photography</h6>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, ea aliquid.
+                    </p>
+                </div>
+            </div>
+
+            <div class="card bp-card" id="bp1">
+                <img src="../Assets/Images/no-picture.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Singko Marias</h5>
+                    <h6 class="card-subtitle">Photography</h6>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, ea aliquid.
+                    </p>
+                </div>
+            </div>
+
+
+
+        </div>
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- <script src="../Assets/JS/bootstrap.bundle.min.js"></script> -->
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous">
+    </script>
+    <script src="../Assets/JS/scrollNavbg.js"></script>
+
+    <!-- Sweetalert JS -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <!-- Redirects User to Book Now -->
+    <script>
+    const bookNowBtns = document.querySelectorAll('.bookNowBtn');
+
+    bookNowBtns.forEach(bookNowBtn => {
+        bookNowBtn.addEventListener("click", function(e) {
+            window.location.href = "/Pages/register.php"
+        });
+    });
+    </script>
+
+</body>
+
+</html>
