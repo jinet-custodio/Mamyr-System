0dba070b22f85ee41f2f47b1694b7a00e85b38ae
jinet-custodio <jeanettemaecustodio.basc@gmail.com>
2025-10-08 01:39:21 +0800

    navbar modifications

--- Pages/about.php
+++ Pages/about.php
@@ -75,27 +75,27 @@ while ($row = $getWebContentResult->fetch_assoc()) {
                         <a class="nav-link" href="../index.php"> Home</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link  dropdown-toggle " href="#" id="navbarDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                             data-bs-toggle="dropdown" aria-expanded="false">
-                            AMENITIES
+                            Amenities
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="../Pages/amenities.php">RESORT AMENITIES</a></li>
-                            <li><a class="dropdown-item" href="ratesAndHotelRooms.php">RATES AND HOTEL ROOMS</a></li>
-                            <li><a class="dropdown-item" href="../Pages/events.php">EVENTS</a></li>
+                            <li><a class="dropdown-item" href="amenities.php">Resort Amenities</a></li>
+                            <li><a class="dropdown-item" href="ratesAndHotelRooms.php">Rates and Hotel Rooms</a></li>
+                            <li><a class="dropdown-item" href="events.php">Events</a></li>
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/Pages/blog.php">BLOG</a>
+                        <a class="nav-link" href="/Pages/blog.php">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/Pages/busPartnerRegister.php" id="bopNav">BE OUR PARTNER</a>
+                        <a class="nav-link" href="/Pages/busPartnerRegister.php" id="bopNav">Be Our Partner</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="./about.php">ABOUT</a>
+                        <a class="nav-link active" href="./about.php">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="register.php">BOOK NOW</a>
+                        <a class="nav-link" href="register.php">Book Now</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="register.php">Sign Up</a>
--- Pages/beOurPartnerNew.php
+++ Pages/beOurPartnerNew.php
@@ -31,27 +31,27 @@
                     <a class="nav-link" href="../index.php"> Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link  dropdown-toggle " href="#" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        AMENITIES
+                        Amenities
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item active" href="amenities.php">RESORT AMENITIES</a></li>
-                        <li><a class="dropdown-item" href="ratesAndHotelRooms.php">RATES AND HOTEL ROOMS</a></li>
-                        <li><a class="dropdown-item" href="events.php">EVENTS</a></li>
+                        <li><a class="dropdown-item" href="amenities.php">Resort Amenities</a></li>
+                        <li><a class="dropdown-item" href="ratesAndHotelRooms.php">Rates and Hotel Rooms</a></li>
+                        <li><a class="dropdown-item" href="events.php">Events</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="blog.php">BLOG</a>
+                    <a class="nav-link" href="blog.php">Blog</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="#" id="bopNav">BE OUR PARTNER</a>
+                    <a class="nav-link active" href="#" id="bopNav">Be Our Partner</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.php">ABOUT</a>
+                    <a class="nav-link" href="about.php">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="register.php">BOOK NOW</a>
+                    <a class="nav-link" href="register.php">Book Now</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="register.php">Sign Up</a>
